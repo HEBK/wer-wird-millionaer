e73dfb900a0d27b1777f57832a9b8bb8aee4a02c
Cr4zyFl1x <florian@kleine-vorholt.net>
2021-02-16 18:15:31 +0100

    WWM: Commit (v0.0.4-alpha) - 2

---
+++ src/eu/flrkv/wwm/Game/Question.java
@@ -0,0 +1,99 @@
+package eu.flrkv.wwm.Game;
+
+public class Question {
+
+    /**
+     * Fragestellung als String
+     */
+    private final String question;
+
+    /**
+     * Eindeutige ID als Integer
+     */
+    private final int id;
+
+    /**
+     * Antworten als Strings in einem Array
+     */
+    private final String[] answer = new String[4];
+
+    /**
+     * Schwierigkeitsgrad als Integer
+     */
+    private final int difficulty;
+
+
+    /**
+     * Konstruktor der Klasse Question
+     * Konstruiert die Frage und setzt alle Klassesattribute
+     *
+     * @param pID Eindeutige ID der Frage
+     * @param pDifficulty Schwierigkeitsgrad der Frage als Integer (1 -> Leicht, 2 -> Mittel, 3 -> Schwer)
+     * @param pQuestion Fragestellung als String
+     * @param pAnswer1 Antwortmöglichkeit als String
+     * @param pAnswer2 Antwortmöglichkeit als String
+     * @param pAnswer3 Antwortmöglichkeit als String
+     * @param pRightAnswer Richtige Antwort als String
+     */
+    public Question(int pID, int pDifficulty, String pQuestion, String pAnswer1, String pAnswer2, String pAnswer3, String pRightAnswer){
+
+        this.id = pID;
+        this.question = pQuestion;
+
+        this.answer[0] = pAnswer1;
+        this.answer[1] = pAnswer2;
+        this.answer[2] = pAnswer3;
+        this.answer[3] = pRightAnswer;
+
+        this.difficulty = pDifficulty;
+    }
+
+    /**
+     * Gibt alle falschen Antworten zurück.
+     * @return falsche Antworten als Array.
+     */
+    public String[] getWrongAnswers()
+    {
+        String[] retAnswers = new String[3];
+        for (int i=0; i < this.answer.length; i++) {
+            retAnswers[i] = answer[i];
+        }
+        return retAnswers;
+    }
+
+    /**
+     * Gibt die richtige Antwort zurück.
+     * @return Richtige Antwort als String
+     */
+    public String getRightAnswer()
+    {
+        return answer[3];
+    }
+
+    /**
+     * Gibt die Fragestellung zurück.
+     * @return Fragestellung als String
+     */
+    public String getQuestion() {
+        return question;
+    }
+
+    /**
+     * Gibt den Schwierigkeitsgrad zurück
+     * 1 -> Leicht
+     * 2 -> Mittel
+     * 3 -> Schwer
+     * @return Schwierigkeitsgrad als Integer
+     */
+    public int getDifficulty() {
+        return difficulty;
+    }
+
+    /**
+     * Gibt die eindeutige ID der Frage zurück.
+     * @return Eindeutige ID als Integer
+     */
+    public int getId() {
+        return id;
+    }
+}
---
+++ src/eu/flrkv/wwm/Game/QuestionController.java
@@ -0,0 +1,123 @@
+package eu.flrkv.wwm.Game;
+
+import eu.flrkv.wwm.Exceptions.QuestionNotFoundException;
+import eu.flrkv.wwm.Storage.DatabaseConnection;
+import eu.flrkv.wwm.Utils.Utils;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class QuestionController {
+
+    /**
+     * Objekt des aktuellen Spiels
+     */
+    private Game game;
+
+    /**
+     * Kontruktor der Klasse QuestionController
+     * @param pGame Objekt der Klasse Game -> Referenzobjekt für die Fragenasuwahl
+     */
+    public QuestionController(Game pGame)
+    {
+        this.game = pGame;
+    }
+
+    /**
+     * Holte eine neu noch nicht verwandte Frage aus der MySQL-Dtaenbanktabelle 'wwm_questions'
+     * @param pDifficulty Schwierigkeitskategorie der Frage (0 -> Leicht, 1 -> Mittel, 2 -> Schwer)
+     * @return Gibt die Frage als Objekt zurück.
+     */
+    public Question getNewQuestion(int pDifficulty)
+    {
+        return null;
+    }
+
+
+
+
+
+    /*
+     * Static section
+     */
+
+
+    /**
+     * Speichert eine neue Frage in die Datenbank
+     * @param pDifficulty Schwierigkeitsgrad (1-3) der Frage
+     * @param pQuestion Frage als String
+     * @param pAnswer0 Antwort als String
+     * @param pAnswer1 Antwort als String
+     * @param pAnswer2 Antwort als String
+     * @param pRightAnswer Richtige Antwort als String
+     * @return Gibt true als boolean zurück, wenn die Frage erfolgreich abgespeichert wurde.
+     */
+    public static boolean addQuestion(int pDifficulty, String pQuestion, String pAnswer0, String pAnswer1, String pAnswer2, String pRightAnswer)
+    {
+        try {
+            PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement("INSERT INTO wwm_questions (difficulty, question, answer0, answer1, answer2, answer3) VALUES (?, ?, ?, ?, ?, ?)");
+            ps.setInt(1, pDifficulty);
+            ps.setString(2, pQuestion);
+            ps.setString(3, pAnswer0);
+            ps.setString(4, pAnswer1);
+            ps.setString(5, pAnswer2);
+            ps.setString(6, pRightAnswer);
+
+            if(ps.executeUpdate() > 0) {
+                return true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * Speichert eine neue Frage in der Datenbank
+     * @param pQuestion Frage als Objekt des Datentyps Frage
+     * @return Gibt true als boolean zurück, wenn die Frage erfolgreich abgespeichert wurde.
+     */
+    public static boolean addQuestion(Question pQuestion)
+    {
+        return addQuestion(pQuestion.getDifficulty(), pQuestion.getQuestion(), pQuestion.getWrongAnswers()[0], pQuestion.getWrongAnswers()[1], pQuestion.getWrongAnswers()[2], pQuestion.getRightAnswer());
+    }
+
+    /**
+     * Holt eine Frage mit einer bestimmten ID aus der Datenbank.
+     * @param pQuestionID ID der Frage, die gelesen werden soll.
+     * @return Gibt die Frage als Objekt zurück. Wenn keine Frage mit der ID existiert wird null zurückgegeben.
+     */
+    public static Question getQuestion(int pQuestionID) throws QuestionNotFoundException {
+        try {
+            PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement("SELECT * FROM wwm_questions WHERE ID = ?");
+            ps.setInt(1, pQuestionID);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return new Question(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7));
+            }
+        } catch(SQLException e) {
+            Utils.consoleLog("ERROR", "A Question with that ID could not be found!");
+            throw new QuestionNotFoundException("A Question with that ID could not be found!");
+        }
+        return null;
+    }
+
+    public static ArrayList<Question> getAllQuestions()
+    {
+        ArrayList<Question> questions = new ArrayList<Question>();
+        try {
+            PreparedStatement ps = DatabaseConnection.getConnection().prepareStatement("SELECT * FROM wwm_questions");
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                questions.add(new Question(rs.getInt(1), rs.getInt(2), rs.getString(3), rs.getString(4), rs.getString(5), rs.getString(6), rs.getString(7)));
+            }
+        } catch (SQLException e) {
+            Utils.consoleLog("ERROR", "Could not read questions from database!");
+            e.printStackTrace();
+        }
+        return questions;
+    }
+
+}
