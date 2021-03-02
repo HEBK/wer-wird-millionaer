1263ee40f846cda1592408facc766f786801d6fb
Cr4zyFl1x <florian@kleine-vorholt.net>
2021-03-02 17:45:22 +0100

    Added: Layout for the Game Window (J.V.)

--- src/eu/flrkv/wwm/GUI/GameWindow.java
+++ src/eu/flrkv/wwm/GUI/GameWindow.java
@@ -8,6 +8,15 @@ import java.awt.*;
 public class GameWindow extends FrameTemplate {
 
 
+    private JPanel panel1;
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JTextPane aTextPane;
+    private JTextPane bTextPane;
+    private JTextPane dTextPane;
+    private JTextPane cTextPane;
 
     public GameWindow(GUIController pController) {
         super("Wer wird Millionär | InGame - @", new Dimension(700, 500));
@@ -16,7 +25,7 @@ public class GameWindow extends FrameTemplate {
         setWindowProperties();
 
         this.setContentPane(new JLabel(new ImageIcon("common/wwm_game_bg.png")));
-
+        this.add(panel1);
     }
 
     private void setWindowProperties()
