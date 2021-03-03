c7d7f4e4e9c8b6de97caf04a3da721726c292f3e
Cr4zyFl1x <florian@kleine-vorholt.net>
2021-03-03 14:58:09 +0100

    Updated: GameWindow UI Progress

--- src/eu/flrkv/wwm/GUI/GameWindow.java
+++ src/eu/flrkv/wwm/GUI/GameWindow.java
@@ -4,36 +4,64 @@ import eu.flrkv.wwm.Utils.Utils;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class GameWindow extends FrameTemplate {
 
 
     private JPanel panel1;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
-    private JTextPane aTextPane;
-    private JTextPane bTextPane;
-    private JTextPane dTextPane;
-    private JTextPane cTextPane;
+    private JLabel logo;
+    private JButton buttonAnswerA;
+    private JButton buttonAnswerB;
+    private JButton buttonAnswerC;
+    private JButton buttonAnswerD;
     private JLabel fiftyFiftyJoker;
+    private JLabel phoneJoker;
+    private JLabel audienceJoker;
+    private JButton hauptmenüButton;
+
 
     public GameWindow(GUIController pController) {
-        super("Wer wird Millionär | InGame - @", new Dimension(700, 500));
+        super("Wer wird Millionär | InGame - @", new Dimension(950, 650));
 
         Utils.consoleLog("INFO", "Game window initialized!");
         setWindowProperties();
 
         // this.setContentPane(new JLabel(new ImageIcon("common/wwm_game_bg.png")));
         this.add(panel1);
+        autoResizeFrame();
+
+
+        audienceJoker.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                super.mouseClicked(e);
+
+                JOptionPane.showConfirmDialog(null, "Publikumsjoker gedrückt");
+            }
+        });
     }
 
     private void setWindowProperties()
     {
-        this.setVisible(true);
         this.setDefaultCloseOperation(FrameTemplate.DISPOSE_ON_CLOSE);
 
     }
 
+    private void createUIComponents() {
+        logo = new JLabel(new ImageIcon("common/logos/wwm_120x120.png"));
+
+        audienceJoker = new JLabel(new ImageIcon("common/icons/jokers/jokerAudience_h64.png"));
+        fiftyFiftyJoker = new JLabel(new ImageIcon("common/icons/jokers/jokerFiftyFifty_h64.png"));
+        phoneJoker = new JLabel(new ImageIcon("common/icons/jokers/jokerPhone_h64.png"));
+    }
+
+
+
+    private void autoResizeFrame()
+    {
+        this.pack();
+        this.setMinimumSize(new Rectangle(getBounds()).getSize());
+    }
 }
